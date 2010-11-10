3b2e5b5ce52a17c9e7964be58da78f3dfad1bc4c
Anthony Lauzon <alauzon@google.com>
2010-11-10 15:24:48 -0500

    more aggressive expirations

--- src/redis.c
+++ src/redis.c
@@ -1331,81 +1331,82 @@ void freeMemoryIfNeeded(void) {
     if (server.maxmemory_policy == REDIS_MAXMEMORY_NO_EVICTION) return;
 
     while (server.maxmemory && zmalloc_used_memory() > server.maxmemory) {
-        int j, k, freed = 0;
+        int i, j, k, freed = 0;
+        for (i = 0; i < REDIS_NUM_DELETE_FREEING; i++) {
+			for (j = 0; j < server.dbnum; j++) {
+				long bestval = 0; /* just to prevent warning */
+				sds bestkey = NULL;
+				struct dictEntry *de;
+				redisDb *db = server.db+j;
+				dict *dict;
 
-        for (j = 0; j < server.dbnum; j++) {
-            long bestval = 0; /* just to prevent warning */
-            sds bestkey = NULL;
-            struct dictEntry *de;
-            redisDb *db = server.db+j;
-            dict *dict;
+				if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_LRU ||
+					server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_RANDOM)
+				{
+					dict = server.db[j].dict;
+				} else {
+					dict = server.db[j].expires;
+				}
+				if (dictSize(dict) == 0) continue;
 
-            if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_LRU ||
-                server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_RANDOM)
-            {
-                dict = server.db[j].dict;
-            } else {
-                dict = server.db[j].expires;
-            }
-            if (dictSize(dict) == 0) continue;
+				/* volatile-random and allkeys-random policy */
+				if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_RANDOM ||
+					server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_RANDOM)
+				{
+					de = dictGetRandomKey(dict);
+					bestkey = dictGetEntryKey(de);
+				}
 
-            /* volatile-random and allkeys-random policy */
-            if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_RANDOM ||
-                server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_RANDOM)
-            {
-                de = dictGetRandomKey(dict);
-                bestkey = dictGetEntryKey(de);
-            }
+				/* volatile-lru and allkeys-lru policy */
+				else if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_LRU ||
+					server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_LRU)
+				{
+					for (k = 0; k < server.maxmemory_samples; k++) {
+						sds thiskey;
+						long thisval;
+						robj *o;
 
-            /* volatile-lru and allkeys-lru policy */
-            else if (server.maxmemory_policy == REDIS_MAXMEMORY_ALLKEYS_LRU ||
-                server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_LRU)
-            {
-                for (k = 0; k < server.maxmemory_samples; k++) {
-                    sds thiskey;
-                    long thisval;
-                    robj *o;
+						de = dictGetRandomKey(dict);
+						thiskey = dictGetEntryKey(de);
+						o = dictGetEntryVal(de);
+						thisval = estimateObjectIdleTime(o);
 
-                    de = dictGetRandomKey(dict);
-                    thiskey = dictGetEntryKey(de);
-                    o = dictGetEntryVal(de);
-                    thisval = estimateObjectIdleTime(o);
+						/* Higher idle time is better candidate for deletion */
+						if (bestkey == NULL || thisval > bestval) {
+							bestkey = thiskey;
+							bestval = thisval;
+						}
+					}
+				}
 
-                    /* Higher idle time is better candidate for deletion */
-                    if (bestkey == NULL || thisval > bestval) {
-                        bestkey = thiskey;
-                        bestval = thisval;
-                    }
-                }
-            }
+				/* volatile-ttl */
+				else if (server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_TTL) {
+					for (k = 0; k < server.maxmemory_samples; k++) {
+						sds thiskey;
+						long thisval;
 
-            /* volatile-ttl */
-            else if (server.maxmemory_policy == REDIS_MAXMEMORY_VOLATILE_TTL) {
-                for (k = 0; k < server.maxmemory_samples; k++) {
-                    sds thiskey;
-                    long thisval;
+						de = dictGetRandomKey(dict);
+						thiskey = dictGetEntryKey(de);
+						thisval = (long) dictGetEntryVal(de);
 
-                    de = dictGetRandomKey(dict);
-                    thiskey = dictGetEntryKey(de);
-                    thisval = (long) dictGetEntryVal(de);
+						/* Expire sooner (minor expire unix timestamp) is better
+						 * candidate for deletion */
+						if (bestkey == NULL || thisval < bestval) {
+							bestkey = thiskey;
+							bestval = thisval;
+						}
+					}
+				}
 
-                    /* Expire sooner (minor expire unix timestamp) is better
-                     * candidate for deletion */
-                    if (bestkey == NULL || thisval < bestval) {
-                        bestkey = thiskey;
-                        bestval = thisval;
-                    }
-                }
-            }
-
-            /* Finally remove the selected key. */
-            if (bestkey) {
-                robj *keyobj = createStringObject(bestkey,sdslen(bestkey));
-                dbDelete(db,keyobj);
-                server.stat_expiredkeys++;
-                decrRefCount(keyobj);
-                freed++;
-            }
+				/* Finally remove the selected key. */
+				if (bestkey) {
+					robj *keyobj = createStringObject(bestkey,sdslen(bestkey));
+					dbDelete(db,keyobj);
+					server.stat_expiredkeys++;
+					decrRefCount(keyobj);
+					freed++;
+				}
+			}
         }
         if (!freed) return; /* nothing to free... */
     }
